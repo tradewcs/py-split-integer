4e340f18c72a9d51c8eadfab66ff6a036efad3eb
Mike <tradewcs@gmail.com>
2025-11-13 08:53:22 +0200

    changed testcases to more explicit

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,20 +6,16 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert all(num == 5 for num in split_integer(25, 5))
+    assert split_integer(25, 5) == [5, 5, 5, 5, 5]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    splited = split_integer(57, 1)
-    assert len(splited) == 1
-    assert splited[0] == 57
+    assert split_integer(57, 1) == [57]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    splited = split_integer(19, 4)
-    assert sorted(splited) == splited
+    assert split_integer(19, 4) == [4, 5, 5, 5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    splited = split_integer(5, 7)
-    assert splited.count(0) == 2
+    assert split_integer(5, 7) == [0, 0, 1, 1, 1, 1, 1]
